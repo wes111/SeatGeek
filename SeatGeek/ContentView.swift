e1c2811f46a33e0c6f73782e83d472befc06bf42
Wes Luntsford <wesleyluntsford@gmail.com>
2022-04-22 14:47:39 -0500

    Initial Commit

---
+++ SeatGeek/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SeatGeek
+//
+//  Created by Wesley Luntsford on 4/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SeatGeek/SeatGeekApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeatGeekApp.swift
+//  SeatGeek
+//
+//  Created by Wesley Luntsford on 4/22/22.
+//
+
+import SwiftUI
+
+@main
+struct SeatGeekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
